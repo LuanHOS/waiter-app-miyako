614d129d943a3b43f19f81c02865f3b42de99345
Luan Henrique Oliveira Sales <LuanHos1998@hotmail.com>
2025-08-13 17:40:13 -0300

    Tela de Ajustes Feita

--- waiter-app-miyako/Views/ConfiguracoesPage.xaml.cs
+++ waiter-app-miyako/Views/ConfiguracoesPage.xaml.cs
@@ -2,8 +2,25 @@ namespace waiter_app_miyako.Views;
 
 public partial class ConfiguracoesPage : ContentPage
 {
-	public ConfiguracoesPage()
-	{
-		InitializeComponent();
-	}
+    public ConfiguracoesPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnDeslogarClicked(object sender, EventArgs e)
+    {
+        // Exibe um pop-up de alerta para confirmação do usuário
+        bool querDeslogar = await DisplayAlert(
+            "Confirmar Logout",
+            "Você tem certeza que deseja deslogar da conta atual?",
+            "SIM",
+            "NÃO");
+
+        // Se o usuário clicou em "SIM" (que retorna true)
+        if (querDeslogar)
+        {
+            // Redireciona o usuário de volta para a tela de Login
+            Application.Current.MainPage = new LoginPage();
+        }
+    }
 }
